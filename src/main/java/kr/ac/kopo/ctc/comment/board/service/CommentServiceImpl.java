9594b95e80139ba6c802ee18031af45afde5f0fc
Jaeyoung Ju <101681367+JAEYOUNGJU@users.noreply.github.com>
2022-07-20 19:08:00 +0900

    @Override 수정

--- src/main/java/kr/ac/kopo/ctc/comment/board/service/CommentServiceImpl.java
+++ src/main/java/kr/ac/kopo/ctc/comment/board/service/CommentServiceImpl.java
@@ -10,26 +10,29 @@ import org.springframework.stereotype.Service;
 import kr.ac.kopo.ctc.comment.board.domain.Comment;
 import kr.ac.kopo.ctc.comment.board.repository.CommentRepository;
 
+//CommentService에 작성한 메서드를 구현하기 위해 CommentServiceImpl 클래스 생성
+
 @Service
-public class CommentServiceImpl implements CommentService {
+public class CommentServiceImpl implements CommentService { //해당 클래스가 CommentService 인터페이스를 구현한다는 의미
 	@Autowired
 	CommentRepository commentRepository;
 
+	
+	public CommentServiceImpl(CommentRepository commentRepository) {
+		this.commentRepository = commentRepository;
+	}
+	
 	@Override
 	public List<Comment> findAll() {
 		return commentRepository.findAll();
 	}
 
 	@Override
-	public List<Comment> findOneById(int id) {
+	public List<Comment> findOneById(Long id) {
 		// TODO Auto-generated method stub
 		return commentRepository.findOneById(id);
 	}
 
-	@Override
-	public void commentSave(Comment comment) {
-		commentRepository.save(comment);
-		
-	}
+
 
 }
