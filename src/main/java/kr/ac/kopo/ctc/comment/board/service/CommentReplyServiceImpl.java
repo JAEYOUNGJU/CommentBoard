89910e4663b1226c751339d1bea9885cfe2f2855
Jaeyoung Ju <101681367+JAEYOUNGJU@users.noreply.github.com>
2022-07-20 20:54:26 +0900

    reply service

--- src/main/java/kr/ac/kopo/ctc/comment/board/service/CommentReplyServiceImpl.java
+++ src/main/java/kr/ac/kopo/ctc/comment/board/service/CommentReplyServiceImpl.java
@@ -1,8 +1,32 @@
 package kr.ac.kopo.ctc.comment.board.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import kr.ac.kopo.ctc.comment.board.domain.CommentReply;
+import kr.ac.kopo.ctc.comment.board.repository.CommentReplyRepository;
+
 
 @Service
 public class CommentReplyServiceImpl implements CommentReplyService {
+	@Autowired
+	CommentReplyRepository commentReplyRepository;
 
+	public CommentReplyServiceImpl(CommentReplyRepository commentReplyRepository) {
+		this.commentReplyRepository = commentReplyRepository;
+	}
+
+	@Override
+	public List<CommentReply> findAll() {
+		return commentReplyRepository.findAll();
+	}
+
+	@Override
+	public List<CommentReply> findOneById(Long id) {
+		// TODO Auto-generated method stub
+		return commentReplyRepository.findOneById(id);
+	}
 }
