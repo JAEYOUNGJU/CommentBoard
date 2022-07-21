cc23cae3e60ffe2d262387477d491110a5a6f086
Jaeyoung Ju <101681367+JAEYOUNGJU@users.noreply.github.com>
2022-07-21 18:30:02 +0900

    import 수정

--- src/main/java/kr/ac/kopo/ctc/comment/board/repository/CommentRepository.java
+++ src/main/java/kr/ac/kopo/ctc/comment/board/repository/CommentRepository.java
@@ -1,15 +1,10 @@
 package kr.ac.kopo.ctc.comment.board.repository;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
-import javax.transaction.Transactional;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
 import kr.ac.kopo.ctc.comment.board.domain.Comment;
 
