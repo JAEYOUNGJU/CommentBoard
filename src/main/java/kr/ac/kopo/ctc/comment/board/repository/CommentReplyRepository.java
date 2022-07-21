0dbf05449dcd88b1581241e133713b3f895d663d
Jaeyoung Ju <101681367+JAEYOUNGJU@users.noreply.github.com>
2022-07-21 18:29:23 +0900

    JpaSpecificationExecutor 인터페이스 상속

--- src/main/java/kr/ac/kopo/ctc/comment/board/repository/CommentReplyRepository.java
+++ src/main/java/kr/ac/kopo/ctc/comment/board/repository/CommentReplyRepository.java
@@ -1,17 +1,21 @@
 package kr.ac.kopo.ctc.comment.board.repository;
 
 import java.util.List;
-import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import kr.ac.kopo.ctc.comment.board.domain.Comment;
 import kr.ac.kopo.ctc.comment.board.domain.CommentReply;
 
-public interface CommentReplyRepository extends JpaRepository<CommentReply, Long> {
+public interface CommentReplyRepository extends JpaRepository<CommentReply, Long>, JpaSpecificationExecutor<CommentReply>  {
 	// selectOne
-	List<CommentReply> findOneById(Long id); // id 반환
+	List<CommentReply> findOneById(Long id);
 
 	// selectOne
-	List<CommentReply> findAllByCommentId(Long id); // id 반환
+	List<CommentReply> findAllByCommentId(Long id);
+
 }
