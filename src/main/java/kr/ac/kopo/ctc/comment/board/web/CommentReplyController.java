efed09e1a8c9c4c672a8be1e1cc66a06bc920a57
Jaeyoung Ju <101681367+JAEYOUNGJU@users.noreply.github.com>
2022-07-22 15:06:32 +0900

    @InitBinder annotaion 추가 및 오타 수정

--- src/main/java/kr/ac/kopo/ctc/comment/board/web/CommentReplyController.java
+++ src/main/java/kr/ac/kopo/ctc/comment/board/web/CommentReplyController.java
@@ -1,10 +1,15 @@
 package kr.ac.kopo.ctc.comment.board.web;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +28,14 @@ public class CommentReplyController {
 	CommentReplyRepository commentReplyRepository;
 	@Autowired
 	CommentReplyService commentReplyService;
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+	}
+	
 	//댓글 입력
-	@RequestMapping(value = "/selectOne/Replyinsert")
+	@RequestMapping(value = "/selectOne/ReplyInsert")
 	public String insertReply(CommentReply commentReply) {
 		commentReplyService.save(commentReply);
 		 return "redirect:/comment/index";
@@ -33,11 +44,11 @@ public class CommentReplyController {
 	}
 	
 	//댓글 삭제
-   
+	@RequestMapping(value = "/ReplyDelete/{id}")
    public String deleteReply(@PathVariable(value="id") Long id) {
 	  commentReplyService.deleteById(id);
       return "redirect:/comment/index";
-   }
+	}
 	   
 
 }
